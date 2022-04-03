682793d7a9b9c7cab69b1bcbc44f8e54f8fb6e24
Anna <ovodova@outlook.com>
2022-04-03 21:56:57 +0700

    added files

---
+++ HW_2_UI/NavIgation/ViewControllers/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  NavIgation
+//
+//  Created by Anna Ovodova on 31.03.2022.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
